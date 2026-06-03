90a478bc79f233cdea34bcb891095dea98f2bc51
DataDevPro <ifcd0211.2021@gmail.com>
2026-06-03 19:30:17 +0100

    Update Main.java

--- java-pruebas/src/Main.java
+++ java-pruebas/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo desde BibliotecaDAM");
+        Syste.out.println("Hola mundo desde BibliotecaDAM")
     }
 }
